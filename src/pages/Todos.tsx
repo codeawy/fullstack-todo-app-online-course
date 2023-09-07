c3a9cf1f9b9f5eab966da5d1a8bc4e82656696e1
codeawy <codeawy@gmail.com>
2023-09-07 14:41:29 +0300

    Sorting by and page size

--- src/pages/Todos.tsx
+++ src/pages/Todos.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { ChangeEvent, useState } from "react";
 import Paginator from "../components/Paginator";
 import Button from "../components/ui/Button";
 import useCustomQuery from "../hooks/useCustomQuery";
@@ -10,18 +10,18 @@ const TodosPage = () => {
   const userData = userDataString ? JSON.parse(userDataString) : null;
 
   const [page, setPage] = useState<number>(1);
+  const [pageSize, setPageSize] = useState<number>(10);
+  const [sortBy, setSortBy] = useState<string>("DESC");
 
   const { isLoading, data, isFetching } = useCustomQuery({
-    queryKey: [`todos-page-${page}`],
-    url: `/todos?pagination[pageSize]=10&pagination[page]=${page}`,
+    queryKey: [`todos-page-${page}`, `${pageSize}`, `${sortBy}`],
+    url: `/todos?pagination[pageSize]=${pageSize}&pagination[page]=${page}&sort=createdAt:${sortBy}`,
     config: {
       headers: {
         Authorization: `Bearer ${userData.jwt}`,
       },
     },
   });
-
-  console.log(isLoading, isFetching);
 
   // ** Handlers
   const onClickPrev = () => {
@@ -30,15 +30,35 @@ const TodosPage = () => {
   const onClickNext = () => {
     setPage(prev => prev + 1);
   };
+  const onChangePageSize = (e: ChangeEvent<HTMLSelectElement>) => {
+    setPageSize(+e.target.value);
+  };
+  const onChangeSortBy = (e: ChangeEvent<HTMLSelectElement>) => {
+    setSortBy(e.target.value);
+  };
 
   if (isLoading) return <h3>Loading..</h3>;
 
   return (
     <section className="max-w-2xl mx-auto">
-      <div className="flex items-center justify-center space-x-2">
+      <div className="flex items-center justify-between space-x-2">
         <Button size={"sm"} onClick={onGenerateTodos} title="Generate 100 records">
           Generate todos
         </Button>
+        <div className="flex items-center justify-between space-x-2 text-md">
+          <select className="border-2 border-indigo-600 rounded-md p-2" value={sortBy} onChange={onChangeSortBy}>
+            <option disabled>Sort by</option>
+            <option value="ASC">Oldest</option>
+            <option value="DESC">Latest</option>
+          </select>
+
+          <select className="border-2 border-indigo-600 rounded-md p-2" value={pageSize} onChange={onChangePageSize}>
+            <option disabled>Page size</option>
+            <option value={10}>10</option>
+            <option value={50}>50</option>
+            <option value={100}>100</option>
+          </select>
+        </div>
       </div>
 
       <div className="my-10">
